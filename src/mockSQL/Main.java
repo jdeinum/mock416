8384c87c8d96cb2d5f2751f6808536a3f7bb4b0a
Ifaz Kabir <ifazk@users.noreply.github.com>
2020-08-17 13:01:21 -0600

    Adding mock sql code.

---
+++ src/mockSQL/Main.java
@@ -0,0 +1,34 @@
+package mockSQL;
+
+import java.util.Random;
+
+public class Main {
+
+    private static void handleSQL(Request request, SQL db, Random rd) {
+        String studentId = request.getParameter("studId");
+        StringBuilder sb = new StringBuilder("SELECT * FROM Students where studId='");
+
+        if (rd.nextBoolean()) {
+            sb.append(studentId);
+        } else {
+            sb.append("123456");
+        }
+
+        sb.append("'");
+
+        String query = sb.toString();
+
+        db.executeQuery(query);
+
+    }
+
+    public static void main(String[] args) {
+        Request req = new Request();
+        SQL db = new SQL();
+        Random rd = new Random();
+
+        System.out.println("Starting SQL handle...");
+        handleSQL(req, db, rd);
+        System.out.println("Ending SQL handle.");
+    }
+}
---
+++ src/mockSQL/SQL.java
@@ -0,0 +1,6 @@
+package mockSQL;
+
+public class SQL {
+    public void executeQuery(String query) {
+    }
+}
