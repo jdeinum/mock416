b4a04b7d4fcfee69350ad2a4986a65e721d58530
Ifaz Kabir <ifazk@users.noreply.github.com>
2020-08-18 02:04:10 -0600

    Fix mock sanitizer.

--- src/mockSQL/SQL.java
+++ src/mockSQL/SQL.java
@@ -3,7 +3,7 @@ package mockSQL;
 public class SQL {
     public static String sanitizeString(String string) {
         // mock sanitization
-        return "42";
+        return string;
     }
 
     public void executeQuery(String query) {
