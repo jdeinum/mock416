bd809895a43adb451b3bc6fac6ef9b43390e1055
Ifaz Kabir <ifazk@users.noreply.github.com>
2020-08-18 02:01:32 -0600

    Adding mock sanitizer.

--- src/mockSQL/Main.java
+++ src/mockSQL/Main.java
@@ -2,9 +2,29 @@ package mockSQL;
 
 import java.util.Random;
 
+import static mockSQL.SQL.sanitizeString;
+
 public class Main {
 
     private static void handleSQL(Request request, SQL db, Random rd) {
+        String studentId = request.getParameter("studId");
+        StringBuilder sb = new StringBuilder("SELECT * FROM Students where studId='");
+
+        if (rd.nextBoolean()) {
+            sb.append(sanitizeString(studentId));
+        } else {
+            sb.append("123456");
+        }
+
+        sb.append("'");
+
+        String query = sb.toString();
+
+        db.executeQuery(query);
+
+    }
+
+    private static void handleSQLBad(Request request, SQL db, Random rd) {
         String studentId = request.getParameter("studId");
         StringBuilder sb = new StringBuilder("SELECT * FROM Students where studId='");
 
--- src/mockSQL/SQL.java
+++ src/mockSQL/SQL.java
@@ -1,6 +1,11 @@
 package mockSQL;
 
 public class SQL {
+    public static String sanitizeString(String string) {
+        // mock sanitization
+        return "42";
+    }
+
     public void executeQuery(String query) {
     }
 }
